832bafa1365c17be0fa626c5fa021fb343571d89
Doug Perez <perezcpt@gmail.com>
2021-03-12 07:32:00 -0800

    Blog section added

---
+++ script.js
@@ -0,0 +1,29 @@
+//Gallery Snap-x
+console.log("SCRIPT CONNECTED");
+const slider = document.querySelector(".swapgal");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", (_) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", (_) => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 2;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
